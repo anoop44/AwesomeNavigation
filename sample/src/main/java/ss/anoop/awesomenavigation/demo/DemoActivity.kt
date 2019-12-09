fb59c0a125e8d2e08e741796e6e0905580c0ff5c
Anoop <anoopvvs@gmail.com>
2019-12-09 16:45:04 +0530

    Sample updated to use the newly added listener

--- sample/src/main/java/ss/anoop/awesomenavigation/demo/DemoActivity.kt
+++ sample/src/main/java/ss/anoop/awesomenavigation/demo/DemoActivity.kt
@@ -1,7 +1,10 @@
 package ss.anoop.awesomenavigation.demo
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.layout_demo_activity.*
+import ss.anoop.awesomenavigation.OnNavigationSelectedListener
 
 class DemoActivity : AppCompatActivity() {
 
@@ -10,5 +13,14 @@ class DemoActivity : AppCompatActivity() {
 
         setContentView(R.layout.layout_demo_activity)
 
+        bottomNavigation.setOnNavigationSelectedListener(object : OnNavigationSelectedListener {
+            override fun onSelectNavigation(id: Int, position: Int) {
+                Log.d("AwesomeNavigation", "onSelectNavigation, id = $id, position = $position")
+            }
+
+            override fun onReselectNavigation(id: Int, position: Int) {
+                Log.d("AwesomeNavigation", "onReselectNavigation, id = $id, position = $position")
+            }
+        })
     }
 }
